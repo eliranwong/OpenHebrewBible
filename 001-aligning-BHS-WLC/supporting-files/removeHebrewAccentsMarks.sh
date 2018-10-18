dcc62a359217f11b8f25bac6cb3391bba14e30cf
Eliran Wong <hkwebtech@gmail.com>
2018-10-18 16:47:04 +0100

    Delete removeHebrewAccentsMarks.sh

--- 001-aligning-BHS-WLC/supporting-files/removeHebrewAccentsMarks.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/sed -E -i .bak -f
-#deal with Hebrew characters in A materials
-s/[\֑\֒\֓\֔\֕\֖\֗\֘\֙\֚\֛\֜\֝\֞\֟\֠\֡\֣\֤\֥\֦\֧\֨\֩\֪\֫\֬\֭\֮\ֽ\ׄ\ׅ\‍\‪\‬\̣\ְ\ֱ\ֲ\ֳ\ִ\ֵ\ֶ\ַ\ָ\ֹ\ֺ\ֻ\ׂ\ׁ\ּ\ֿ\ \(\)\[\]\*\־\׀\׃\׆]//g
-#check the following line, use it only if necessary
-#s/[שׂשׁ]/ש/g
